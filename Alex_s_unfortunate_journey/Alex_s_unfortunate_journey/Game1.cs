fcc105ba39f1ef661df9a377a36a1a2ec744b99b
Mohamed-Amin <mamine22123@gmail.com>
2023-01-05 14:32:11 +0100

    essai

--- Alex_s_unfortunate_journey/Alex_s_unfortunate_journey/Game1.cs
+++ Alex_s_unfortunate_journey/Alex_s_unfortunate_journey/Game1.cs
@@ -1,6 +1,13 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+//using MonoGame.Extended.Animations;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+
 
 namespace Alex_s_unfortunate_journey
 {
@@ -8,6 +15,13 @@ namespace Alex_s_unfortunate_journey
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        //map
+        private TiledMap _tiledMap;
+        private TiledMapRenderer _tiledMapRenderer;
+
+        //perso
+        private Vector2 _positionPerso;
+        private AnimatedSprite _persoIdle;
 
         public Game1()
         {
@@ -19,6 +33,11 @@ namespace Alex_s_unfortunate_journey
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            //map
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            _graphics.PreferredBackBufferWidth = 1200;
+            _graphics.PreferredBackBufferHeight = 768;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -26,7 +45,9 @@ namespace Alex_s_unfortunate_journey
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            //map
+            _tiledMap = Content.Load<TiledMap>("niveauDepart2");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
             // TODO: use this.Content to load your game content here
         }
 
@@ -34,7 +55,9 @@ namespace Alex_s_unfortunate_journey
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //map
+            _tiledMapRenderer.Update(gameTime);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -42,10 +65,14 @@ namespace Alex_s_unfortunate_journey
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // TODO: Add your drawing code here
-
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            //map
+            _tiledMapRenderer.Draw();
+            //perso
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_persoIdle, _positionPerso);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
