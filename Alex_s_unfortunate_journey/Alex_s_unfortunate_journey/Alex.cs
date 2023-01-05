a1f1e187001b9624477e56e247122897fc07a21e
levetma <manon.levet2004@gmail.com>
2023-01-05 15:36:03 +0100

    classe alex

---
+++ Alex_s_unfortunate_journey/Alex_s_unfortunate_journey/Alex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoGame.Extended.Animations;
+
+namespace Alex_s_unfortunate_journey
+{
+    public class Alex
+    {
+        private AnimatedSprite _idle;
+        private AnimatedSprite _idle_left;
+        private AnimatedSprite _walk;
+        private AnimatedSprite _walk_left;
+        private AnimatedSprite _death;
+        private AnimatedSprite _death_left;
+        private AnimatedSprite _jump;
+        private AnimatedSprite _jump_left;
+        
+    }
+}
